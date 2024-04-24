0572c3da3fbf46f6fbae56a5c7d63e532b533c3a
LappyTon <ganailyu4ok@gmail.com>
2024-04-24 23:43:48 +0300

    not workin correct, i will do it tomorrow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// 2048
+// https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/javascript
 // ^^^^^^ LINK THERE ^^^^^^`
+// Number of trailing zeros of N!
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+function zeros(n) {
+  let fact = factorial(n);
+  console.log(fact);
+  let result = 0;
+  for (let i = fact.toString().length - 1; i >= 0; i--) {
+    if (fact.toString()[i] === '0') {
+      result++;
+    } else {
+      break;
+    }
+  }
+  return result;
+}
 
 
   // tasks:
