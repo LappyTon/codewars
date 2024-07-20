bb0419aba6bc69b5821a5e7697dcff105f1534b0
LappyTon <ganailyu4ok@gmail.com>
2024-07-20 22:52:27 +0300

    Breadcrumb Generator start

--- js/script.js
+++ js/script.js
@@ -1,40 +1,11 @@
 // https://www.codewars.com/kata/5a5db0f580eba84589000979
+// Breadcrumb Generator, no link
 //  ^^^ LINK HERE ^^^
 
-let num = 0;
-
-function deleteNum() {
-  num = 0
-}
-function addNumber(number) {
-  return num.toString+number.toString()
-}
 
 
-function minus(number) {
-  return num=-number
-}
-function plus(number) {
-  return num=+number
-}
-function devide(number) {
-  return num=num/number
-}
-function multiply(number) {
-  return num=num*number
-}
-function floor() {
-  return num=Math.floor(num)
-}
-function square() {
-  return num=num^2
-}
-function degree(number) {
-  return num=num^number
-}
-function percent(number) {
-  return num*(1-(number/100))
-}
+
+
 
 
 
